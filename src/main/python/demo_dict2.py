143629929588ec1244de77fa9101119162c683e1
Administrator <Administrator@STO-102PC11.TRAINING.local>
2019-10-28 15:31:16 +0100

    multi level data strucutres

---
+++ src/main/python/demo_dict2.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python3
+
+'''
+
+'''
+
+import pprint
+
+movies = {'olof': ['braveheart', 'frozen', 'tangled'],
+          'josefine': 'pride & prejudice',
+          'jorgen': 'godfather',
+          'thomas': 'the right stuff',
+          'lasse': 'spice girls'}
+
+print(movies)
+print(movies.keys())
+print(movies.values())
+
+
+
+print('######## keys() ##########')
+for name in movies.keys():
+    #print(name + str(movies[name]), end='\n\n')
+    print(f'name = {name}, movie = {movies[name]}')
+
+print('######## values() ##########')
+for movie in movies.values():
+    print(movie)
+
+print('######## items() ##########')
+for name, movie in movies.items():
+    print(movie)
+    print(name)
+
+
+print('######## pprint ##########')
+pprint.pprint(movies)
+
