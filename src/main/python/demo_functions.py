7c3c9190a368685eda8daee9b9ead710cc2e58a1
Administrator <Administrator@STO-102PC11.TRAINING.local>
2019-10-29 15:01:33 +0100

    default parameter

--- src/main/python/demo_functions.py
+++ src/main/python/demo_functions.py
@@ -3,7 +3,7 @@
 '''
 '''
 
-def say_hello(greeting, recipiet="everyone"):
+def say_hello(greeting="Awright", recipiet="everyone"):
     message = greeting + " " + recipiet
     print(message)
 
@@ -12,6 +12,7 @@ def main():
     say_hello("Hello", "Sweden")
     say_hello("Good morning", "Unitied Kingdom")
     say_hello("Hi")
+    say_hello()
 
 
 if __name__ == "__main__":
