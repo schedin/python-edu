0216d6f0532b0d5c3912933de02305524cdd7038
Administrator <Administrator@STO-102PC11.TRAINING.local>
2019-10-29 16:13:10 +0100

    ex8_2

---
+++ src/main/python/ex8_2.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python3
+
+'''
+'''
+
+import os
+
+start_cpu_time = 0
+
+
+def start_timer():
+    global start_cpu_time
+    times = os.times()
+    start_cpu_time =  times[0] + times[1]
+    
+
+def end_timer():
+    global start_cpu_time
+    times = os.times()
+    end_cpu_time =  times[0] + times[1]
+    elapsed_time = end_cpu_time - start_cpu_time
+    print(f'Elapsed time: {elapsed_time:12.3}')
+
+
+def main():
+    start_timer()
+    count = 0
+    for row in open("words"):
+        count += len(row)
+
+    end_timer()
+    
+    print("Number of chars: ", count)
+
+
+if __name__ == "__main__":
+    main()
