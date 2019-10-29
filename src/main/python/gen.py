144d150ee854072bb584b9a888d3ba064601ab4f
Administrator <Administrator@STO-102PC11.TRAINING.local>
2019-10-29 16:31:16 +0100

    start of gen

---
+++ src/main/python/gen.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+
+'''
+'''
+
+
+def frange(start, stop, step=0.25):
+    
+    if not step:
+        return # TODO throw exception
+    
+    
+
+
+def main():
+    print(list(frange(1.1, 3)))
+    
+
+if __name__ == "__main__":
+    main()
