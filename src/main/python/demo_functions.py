df004569961d7e4e5e5660bf2866d7a167350a99
Administrator <Administrator@STO-102PC11.TRAINING.local>
2019-10-29 15:08:24 +0100

    more demo functions

--- src/main/python/demo_functions.py
+++ src/main/python/demo_functions.py
@@ -1,16 +1,21 @@
 #!/usr/bin/env python3
+# coding=utf-8
 
 '''
 '''
+
+import sys
 
 def say_hello(greeting="Awright", recipiet="everyone"):
     message = greeting + " " + recipiet
-    print(message)
+    print(message, sep=" ", end="\n", file=sys.stdout)
 
 
 def main():
     say_hello("Hello", "Sweden")
-    say_hello("Good morning", "Unitied Kingdom")
+    say_hello(greeting="Good morning", recipiet="Unitied Kingdom")
+    say_hello(recipiet="kompis", greeting="Läget")
+    say_hello("Good evning", recipiet="Stockholm")
     say_hello("Hi")
     say_hello()
 
