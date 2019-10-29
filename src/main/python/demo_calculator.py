e14cbd9f7e4f8ba36232eb89402e1fecc6726f66
Administrator <Administrator@STO-102PC11.TRAINING.local>
2019-10-29 15:50:53 +0100

    lambda

--- src/main/python/demo_calculator.py
+++ src/main/python/demo_calculator.py
@@ -15,12 +15,20 @@ def add(x, z):
 def divide(x, z):
     return float(x/z)
 
+add_l = lambda x, z: x + z
+
 
 def main():
     print(f"3 * 4 is {multiply(3, 4)}")
     print(f"3 + 4 is {add(3, 4)}")
     print(f"4 / 3 is {divide(4, 3)}")
+    print(f"3 + 4 is {add_l(3, 4)}")
+    
     
 
 if __name__ == "__main__":
     main()
+
+print(dir())
+print(type(add))
+print(type(add_l))
