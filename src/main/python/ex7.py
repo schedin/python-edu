e1646a9333a7ea90cd715cb66e7e436998b7fbde
Administrator <Administrator@STO-102PC11.TRAINING.local>
2019-10-29 14:14:59 +0100

    restore ex7

--- src/main/python/ex7.py
+++ src/main/python/ex7.py
@@ -8,10 +8,32 @@ import re
 
 def main():
     
+    unused_port_numbers = set()
     
-    
-    pass
-    
+    with open(r'C:\Windows\System32\drivers\etc\services') as fh:
+        for line in fh:
+            if line.startswith("#"):
+                continue
+            
+            line = line.strip()
+            if not line:
+                continue
+            
+            # Remove comments
+            line = re.sub(r'#.*$', "", line)
+
+
+            m = re.search(r'(\d+)/(udp|tcp)\s*(.*)', line)
+            if m:
+                port_number = int(m.group(1))
+                alias = m.group(3).strip()
+                if port_number <= 200 and alias:
+                    unused_port_numbers.add(port_number)
+                
+
+    for port_number in sorted(unused_port_numbers):
+        print(port_number)
+
 if __name__ == "__main__":
     main()
 
