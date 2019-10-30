aa149458155add405a50227cab6bb57a2c9016a3
Administrator <Administrator@STO-102PC11.TRAINING.local>
2019-10-30 11:23:24 +0100

    package/module

---
+++ src/main/python/finance/accounts.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+
+'''
+
+'''
+bank_account = "01234666"
+sort_code = "80-45-37"
+
+
+
+if __name__ == "__main__":
+    print(f"Back account is {bank_account}, sort code is {sort_code}")
+
---
+++ src/main/python/finance/banking.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+
+'''
+'''
+
+from accounts import *
+
+bank_account = "Closed"
+
+print(f"Your bank account is {bank_account}")
+print(f"Your bank account is {sort_code}")
