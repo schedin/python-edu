d9023e56f333b4153f9660d8c4d26f175d28640b
Administrator <Administrator@STO-102PC11.TRAINING.local>
2019-10-30 14:56:56 +0100

    exception handling

--- src/main/python/demo_file_reading.py
+++ src/main/python/demo_file_reading.py
@@ -5,16 +5,21 @@
 '''
 
 import os
+import sys
 
 def main():
     pythonfileDir = os.path.dirname(__file__)
     #filename = os.path.normpath(pythonfileDir + r'/../resources/movies.txt')
     filename = r'words'
+    try:
+        with open(filename, encoding='utf_8') as fh:
+            for line in fh:
+                line = line.strip()
+                print(line)
+    except FileNotFoundError as e:
+        print(f"Could not open file: {str(e)}")
+        sys.exit(1)
     
-    with open(filename, encoding='utf_8') as fh:
-        for line in fh:
-            line = line.strip()
-            print(line)
     
 
 
