ba1fd0ab63e1cf5145a4148dfe71615cfd251ed7
Administrator <Administrator@STO-102PC11.TRAINING.local>
2019-10-30 09:26:33 +0100

    generator

--- src/main/python/demo_generator_functions.py
+++ src/main/python/demo_generator_functions.py
@@ -22,6 +22,12 @@ def generate_numbers():
     for x in range(10):
         yield x
     
+    
+def generate_numbers2():
+    print("Executing generate_numbers2()")
+    
+    return (x for x in range(10))
+    
 
 def main():
     for z in get_numbers():
@@ -30,6 +36,9 @@ def main():
 
     for z in generate_numbers():
         print(z)
+
+    for z in generate_numbers2():
+        print(z)        
 
 if __name__ == "__main__":
     main()
